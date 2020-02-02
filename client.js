4b2bf3772895b7c0bc5e53356c52b7e15094d448
Mulki Ali <mulki.ali@hotmail.fi>
2020-02-02 17:27:24 -0600

    logged

---
+++ client.js
@@ -0,0 +1,26 @@
+$(document).ready();
+
+const employees =[ {
+  firstName: 'Jen',
+  lastName: 'Barber',
+  id: '4521',
+  jobTitle: 'Team Lead',
+  annualSalary: '80000',
+
+},
+{
+  firstName: 'Maurice',
+  lastName: 'Moss',
+  id: '8724',
+  jobTitle: 'Support Team',
+  annualSalary: '58000',
+},
+{
+  firstName: 'Roy',
+  lastName: 'Smith',
+  id: '9623',
+  jobTitle: 'Quality Assurance',
+  annualSalary: '48000',
+},
+];
+console.log('employees', employees);
