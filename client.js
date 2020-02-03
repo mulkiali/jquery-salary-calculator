0edfedb04209721a9d86d650ff25fcce04f7c957
Mulki Ali <mulki.ali@hotmail.fi>
2020-02-02 21:59:00 -0600

    commit

--- client.js
+++ client.js
@@ -1,26 +1,48 @@
-$(document).ready();
+$(document).ready(onReady);
+console.log('js');
 
-const employees =[ {
-  firstName: 'Jen',
-  lastName: 'Barber',
-  id: '4521',
-  jobTitle: 'Team Lead',
-  annualSalary: '80000',
 
-},
-{
-  firstName: 'Maurice',
-  lastName: 'Moss',
-  id: '8724',
-  jobTitle: 'Support Team',
-  annualSalary: '58000',
-},
-{
-  firstName: 'Roy',
-  lastName: 'Smith',
-  id: '9623',
-  jobTitle: 'Quality Assurance',
-  annualSalary: '48000',
-},
-];
-console.log('employees', employees);
+// const employees =[ {
+//   firstName: 'Jen',
+//   lastName: 'Barber',
+//   id: '4521',
+//   jobTitle: 'Team Lead',
+//   annualSalary: '80000',
+//
+// },
+// {
+//   firstName: 'Maurice',
+//   lastName: 'Moss',
+//   id: '8724',
+//   jobTitle: 'Support Team',
+//   annualSalary: '58000',
+// },
+// {
+//   firstName: 'Roy',
+//   lastName: 'Smith',
+//   id: '9623',
+//   jobTitle: 'Quality Assurance',
+//   annualSalary: '48000',
+// },
+// ];
+
+
+let employees = [];
+
+function onReady(){
+  console.log('in onReady');
+  $('#submitButton').on('click', addEmployee);
+};
+
+function addEmployee(){
+  console.log('in addEmployee');
+  let newEmployee = {
+    firstName: $( '#inFirstName' ).val(),
+    lastName: $( '#inLastName' ).val(),
+    id: $( '#inID' ).val(),
+    jobTitle: $( '#inJobTitle' ).val(),
+    annualSalary: $( '#inSalary' ).val()
+  }
+  console.log('adding:', newEmployee);
+  
+}
